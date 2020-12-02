539daa429ef46c3e1cefcb084016c10999b20258
Richard Neil Ilagan (Carding) <705310+richardneililagan@users.noreply.github.com>
2020-12-02 07:50:41 +0000

    chore: adds silent one-off benchmark option

--- src/benchmark.js
+++ src/benchmark.js
@@ -2,4 +2,30 @@ const { benchmark } = require('./index')
 
 // :: ---
 
-benchmark()
+const [, , ...args] = process.argv
+
+if (args.length === 0) {
+  // :: move process to CLI wizard
+  benchmark().then(() => process.exit(0))
+} else {
+  // :: else, expect a solution number and difficulty
+  const [problemId, difficulty] = args
+
+  const { algorithms } = require('./algorithms')
+  const solution = algorithms.find((algo) => algo.key === problemId)
+
+  if (!solution) {
+    // :: no solution for that yet,
+    //    or problem ID not found
+    console.error(`Problem ID ${problemId} was not found.`)
+    process.exit(1)
+  }
+
+  const timer = process.hrtime()
+  solution.solver(difficulty).then(() => {
+    const [seconds, nanos] = process.hrtime(timer)
+    const elapsed = seconds + nanos / 1e9
+    console.log(elapsed)
+    process.exit(0)
+  })
+}
