1dfe9980559cf5aeb17e00491060fdb2acf43e93
Richard Neil Ilagan (Carding) <705310+richardneililagan@users.noreply.github.com>
2020-12-04 06:22:24 +0000

    feat: solves 004 P1

--- src/algorithms/004/index.js
+++ src/algorithms/004/index.js
@@ -21,7 +21,32 @@ const solver = async (difficulty) => {
 }
 
 const __easysolver = async (lines) => {
-  // :: TODO
+  const REQUIRED_FIELDS = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+
+  const passports = lines.reduce(
+    (a, line) => {
+      // :: if we encounted a blank line, prep a new document struct
+      if (line.trim().length === 0) return [...a, []]
+
+      // :: get each line's declared fields, and trash the value
+      const fragments = line
+        .split(' ')
+        .map((fragment) => fragment.split(':')[0])
+
+      // :: ... aaaaaand collect
+      a[a.length - 1].push(...fragments)
+      return a
+    },
+    [[]]
+  )
+
+  // :: then just run each passport's fields through validation ---
+  //    each of the required fields should be accounted for
+  const valids = passports.filter((passport) =>
+    REQUIRED_FIELDS.every((field) => passport.includes(field))
+  )
+
+  return valids.length
 }
 
 const __hardsolver = async (lines) => {
