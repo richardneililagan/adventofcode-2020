6031d6f513bb526bfe2d9106c3e101d7735d76ac
Richard Neil Ilagan (Carding) <705310+richardneililagan@users.noreply.github.com>
2020-12-10 07:48:15 +0000

    feat: solves 010 P2

--- src/algorithms/010/index.js
+++ src/algorithms/010/index.js
@@ -18,6 +18,20 @@ const solver = async (difficulty) => {
   }
 }
 
+const __memo = (fn) => {
+  const __cache = {}
+
+  return (args) => {
+    const key = String(args) // :: super rudimentary
+
+    if (!__cache[key]) {
+      __cache[key] = fn(args)
+    }
+
+    return __cache[key]
+  }
+}
+
 const __easysolver = async (lines) => {
   const ratings = lines.map((line) => +line).sort((a, b) => a - b) // :: sort ascending
   ratings.push(ratings[ratings.length - 1] + 3) // :: add the rating of our device
@@ -35,7 +49,29 @@ const __easysolver = async (lines) => {
 }
 
 const __hardsolver = async (lines) => {
-  //
+  const ratings = lines.map((line) => +line).sort((a, b) => a - b)
+  // ::  no need to push the last adapter
+
+  // :: recursively count possible combinations
+  // :: this assumes `adapters` is sorted ascending,
+  //    and all elements are greater than currentJoltage
+  const recurse = __memo((input) => {
+    if (input.length <= 1) return 1
+    const [current, ...adapters] = input
+
+    const combinations = [0, 1, 2]
+      .filter((n) => n < adapters.length)
+      .reduce((a, v) => {
+        const candidateAdapter = adapters[v]
+        if (candidateAdapter - 3 > current) return a // :: this is an incompatible adapter
+
+        return a + recurse([...adapters].splice(v))
+      }, 0)
+
+    return combinations
+  })
+
+  return recurse([0, ...ratings])
 }
 
 module.exports = { solver, name: PROBLEM_NAME }
--- src/algorithms/010/index.test.js
+++ src/algorithms/010/index.test.js
@@ -49,10 +49,42 @@ it('solves the easy example', async () => {
   expect(answer).toBe(220)
 })
 
-// it('solves the hard example', async () => {
-//   const TEST_DATA = []
-//   readFileToArray.mockResolvedValue(TEST_DATA)
-//
-//   const answer = await solver('hard')
-//   expect(answer).toBe(0)
-// })
+it('solves the hard example', async () => {
+  const TEST_DATA = [
+    28,
+    33,
+    18,
+    42,
+    31,
+    14,
+    46,
+    20,
+    48,
+    47,
+    24,
+    23,
+    49,
+    45,
+    19,
+    38,
+    39,
+    11,
+    1,
+    32,
+    25,
+    35,
+    8,
+    17,
+    7,
+    9,
+    4,
+    2,
+    34,
+    10,
+    3,
+  ]
+  readFileToArray.mockResolvedValue(TEST_DATA)
+
+  const answer = await solver('hard')
+  expect(answer).toBe(19208)
+})
