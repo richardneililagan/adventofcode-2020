4701160f2933773e8afb79bdd1fab6d52641349f
Richard Neil Ilagan (Carding) <705310+richardneililagan@users.noreply.github.com>
2020-12-11 15:35:29 +0000

    feat: solves 011 P2

--- src/algorithms/011/index.js
+++ src/algorithms/011/index.js
@@ -88,7 +88,75 @@ const __easysolver = async (lines) => {
 }
 
 const __hardsolver = async (lines) => {
-  //
+  const LAYOUT_WIDTH = lines[0].length
+  const LAYOUT_HEIGHT = lines.length
+  const DELTA_VECTORS = [-1, 0, 1]
+    .reduce((a, x) => [...a, ...[-1, 0, 1].map((y) => [x, y])], [])
+    .filter(([x, y]) => !(x === 0 && y === 0))
+
+  // :: essentially same modus operandi
+  const layout = lines.map((line, y) =>
+    Array(...line).map((char, x) => {
+      // :: ... it just differs on how we're parsing [adjacent] seats
+      if (char !== 'L') return null
+
+      const adjacents = DELTA_VECTORS.reduce((a, [dx, dy]) => {
+        let k = 1
+        let [kx, ky] = [dx, dy]
+        while (
+          x + kx >= 0 &&
+          x + kx < LAYOUT_WIDTH &&
+          y + ky >= 0 &&
+          y + ky < LAYOUT_HEIGHT
+        ) {
+          const [px, py] = [x + kx, y + ky]
+          if (lines[py][px] === 'L') return [...a, [px, py]]
+          // :: ---
+          k += 1
+          ;[kx, ky] = [dx * k, dy * k]
+        }
+
+        // :: if we reach here, we didn't find a seat in this direction
+        return a
+      }, [])
+
+      return { x, y, adjacents, occupied: false }
+    })
+  )
+
+  // :: parse into flat structure
+  const seats = layout.reduce((a, row) => [...a, ...row.filter((s) => s)], [])
+
+  // :: LET'S GO AGAAAAAAAAAAAIN
+  const tick = () => {
+    const switchingSeats = seats
+      .map((seat) => {
+        // ###
+        seat.adjacents.forEach(([x, y]) => {
+          const s = layout[y][x]
+          if (!s) console.log(x, y, seat, s)
+        })
+        const povStates = seat.adjacents.map(([x, y]) => layout[y][x].occupied)
+        const occupieds = povStates.filter((state) => state).length
+
+        if (seat.occupied && occupieds >= 5) return [seat.x, seat.y]
+        else if (!seat.occupied && occupieds === 0) return [seat.x, seat.y]
+        else return null
+      })
+      .filter((seat) => seat)
+
+    switchingSeats.forEach(([x, y]) => {
+      layout[y][x].occupied = !layout[y][x].occupied
+    })
+
+    return switchingSeats.length
+  }
+
+  while (tick()) {
+    // :: grab a pizza
+  }
+
+  return seats.filter((seat) => seat.occupied).length
 }
 
 module.exports = { solver, name: PROBLEM_NAME }
--- src/algorithms/011/index.test.js
+++ src/algorithms/011/index.test.js
@@ -28,10 +28,21 @@ it('solves the easy example', async () => {
   expect(answer).toBe(37)
 })
 
-// it('solves the hard example', async () => {
-//   const TEST_DATA = []
-//   readFileToArray.mockResolvedValue(TEST_DATA)
-//
-//   const answer = await solver('hard')
-//   expect(answer).toBe(0)
-// })
+it('solves the hard example', async () => {
+  const TEST_DATA = [
+    'L.LL.LL.LL',
+    'LLLLLLL.LL',
+    'L.L.L..L..',
+    'LLLL.LL.LL',
+    'L.LL.LL.LL',
+    'L.LLLLL.LL',
+    '..L.L.....',
+    'LLLLLLLLLL',
+    'L.LLLLLL.L',
+    'L.LLLLL.LL',
+  ]
+  readFileToArray.mockResolvedValue(TEST_DATA)
+
+  const answer = await solver('hard')
+  expect(answer).toBe(26)
+})
