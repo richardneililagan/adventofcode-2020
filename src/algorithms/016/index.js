1f09621977c197ad2cf07b24ef72e3dbe9e7f18c
Richard Neil Ilagan (Carding) <705310+richardneililagan@users.noreply.github.com>
2020-12-17 03:38:40 +0000

    feat: solves 016 P1

---
+++ src/algorithms/016/index.js
@@ -0,0 +1,73 @@
+const { getInputFile, readFileToArray } = require('../../helpers/files')
+
+// :: ---
+
+const PROBLEM_NAME = 'Day 16 - Ticket Translation'
+
+const solver = async (difficulty) => {
+  const inputfile = getInputFile(__dirname, difficulty)
+  const lines = await readFileToArray(inputfile)
+
+  switch (difficulty) {
+    case 'easy':
+      return await __easysolver(lines)
+    case 'hard':
+      return await __hardsolver(lines)
+    default:
+      return null
+  }
+}
+
+const __parseRules = (lines) => {
+  let curindex = 0
+  let sink = []
+
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i] === '') {
+      curindex += 1
+      i += 1
+      continue
+    }
+
+    if (!sink[curindex]) sink[curindex] = []
+    sink[curindex].push(lines[i])
+  }
+
+  const [rules, myTicket, nearbyTickets] = sink
+
+  return [
+    rules.map((rule) => {
+      const [rulename, conditions] = rule.split(': ')
+      return { rulename, conditions: conditions.split(' or ') }
+    }),
+    ...myTicket.map((ticket) => ticket.split(',').map(Number)),
+    nearbyTickets.map((ticket) => ticket.split(',').map(Number)),
+  ]
+}
+
+const __easysolver = async (lines) => {
+  // :: composer function to check for validity of a number with respect to a rule
+  const RULECHECK = (min, max) => (n) => n >= min && n <= max
+  const [rules, myTicket, nearbyTickets] = __parseRules(lines)
+
+  const validators = rules.reduce((a, { conditions }) => {
+    return [
+      ...a,
+      ...conditions.map((c) => {
+        const [min, max] = c.split('-')
+        return RULECHECK(Number(min), Number(max))
+      }),
+    ]
+  }, [])
+
+  return nearbyTickets.reduce((a, ticket) => {
+    const invalid = ticket.find((n) => !validators.some((v) => v(n)))
+    return invalid === undefined ? a : a + invalid
+  }, 0)
+}
+
+const __hardsolver = async (lines) => {
+  //
+}
+
+module.exports = { solver, name: PROBLEM_NAME }
---
+++ src/algorithms/016/index.test.js
@@ -0,0 +1,39 @@
+const { solver } = require('./index')
+
+// :: ---
+
+jest.mock('../../helpers/files')
+const { readFileToArray } = require('../../helpers/files')
+
+beforeEach(() => {
+  readFileToArray.mockReset()
+})
+
+it('solves the easy example', async () => {
+  const TEST_DATA = [
+    'class: 1-3 or 5-7',
+    'row: 6-11 or 33-44',
+    'seat: 13-40 or 45-50',
+    '',
+    'your ticket:',
+    '7,1,14',
+    '',
+    'nearby tickets:',
+    '7,3,47',
+    '40,4,50',
+    '55,2,20',
+    '38,6,12',
+  ]
+  readFileToArray.mockResolvedValue(TEST_DATA)
+
+  const answer = await solver('easy')
+  expect(answer).toBe(71)
+})
+
+// it('solves the hard example', async () => {
+//   const TEST_DATA = []
+//   readFileToArray.mockResolvedValue(TEST_DATA)
+//
+//   const answer = await solver('hard')
+//   expect(answer).toBe(0)
+// })
